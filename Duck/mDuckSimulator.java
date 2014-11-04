f98f82bb2d5299f377b21aa89b9b6e6a20e66833
keakalamudo <kelo.akalamudo@gmail.com>
2014-11-04 09:10:35 -0800

    initial commit

---
+++ Duck/mDuckSimulator.java
@@ -0,0 +1,8 @@
+package hfdp.ducks;
+public class mDuckSimulator{
+  public static void main(String[] args){
+    Duck mallard = new MallardDuck();
+    mallard.performQuack();
+    mallard.performFly();
+  }
+}
