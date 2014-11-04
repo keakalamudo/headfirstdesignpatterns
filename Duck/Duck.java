cd3791cf8b026992a78363ab1604cf26fa44d851
keakalamudo <kelo.akalamudo@gmail.com>
2014-11-04 08:19:23 -0800

    initial commit

---
+++ Duck/Duck.java
@@ -0,0 +1,21 @@
+public abstract class Duck{
+  FlyBehavior flyBehavior;
+  QuckBehavior quackBehavior;
+
+  public Duck(){
+
+  }
+  public abstract void display();
+
+  public void perfromFly(){
+    flyBehavior.fly();
+  }
+
+  public void perfromQuack(){
+    quackBehavior.quack();
+  }
+
+  public void swim(){
+    System.out.println("All ducks float even decoys!");
+  }
+}
